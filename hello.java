060ac3268141a51e24d90fbea7eb202ffc7ea392
Aman-1407 <Aman-1407@github.com>
2022-09-13 05:00:30 -0700

    Code added

---
+++ hello.java
@@ -0,0 +1,9 @@
+public class hello {
+	   public static void main(String[] args)
+       {
+               System.out.println("Hello all");
+	       System.out.println("HI");
+	       System.out.println("hhhh////");
+       }
+
+}
